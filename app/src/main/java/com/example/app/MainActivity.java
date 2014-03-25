d79578c26b0d5f041508dc735c1d2e1746440109
Trond Kjetil Bremnes <tkbremnes@gmail.com>
2014-03-25 19:28:35 +0100

    finished

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.app;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v4.content.WakefulBroadcastReceiver;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -20,7 +31,18 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class MainActivity extends Activity {
@@ -31,7 +53,6 @@ public class MainActivity extends Activity {
 
     String SENDER_ID = "964216151419";
 
-    TextView mDisplay;
     GoogleCloudMessaging gcm;
     AtomicInteger msgId = new AtomicInteger();
     SharedPreferences prefs;
@@ -57,20 +78,19 @@ public class MainActivity extends Activity {
 
         context = getApplicationContext();
 
+
+
         // Check device for Play Services APK. If check succeeds, proceed with
         //  GCM registration.
         if (checkPlayServices()) {
             gcm = GoogleCloudMessaging.getInstance(this);
             regid = getRegistrationId(context);
 
-            if (regid.isEmpty()) {
-                registerInBackground();
-            }
+            registerInBackground();
         } else {
             Log.i(TAG, "No valid Google Play Services APK found.");
         }
     }
-
 
     /**
      * Gets the current registration ID for application on GCM service.
@@ -148,7 +168,7 @@ public class MainActivity extends Activity {
                     // so it can use GCM/HTTP or CCS to send messages to your app.
                     // The request to your server should be authenticated if your app
                     // is using accounts.
-                    sendRegistrationIdToBackend();
+                    sendRegistrationIdToBackend(regid);
 
                     // For this demo: we don't need to send it because the device
                     // will send upstream messages to a server that echo back the
@@ -167,7 +187,6 @@ public class MainActivity extends Activity {
 
             @Override
             protected void onPostExecute(String msg) {
-                mDisplay.append(msg + "\n");
             }
         }.execute(null, null, null);
     }
@@ -178,8 +197,30 @@ public class MainActivity extends Activity {
      * device sends upstream messages to a server that echoes back the message
      * using the 'from' address in the message.
      */
-    private void sendRegistrationIdToBackend() {
-        // Your implementation here.
+    private void sendRegistrationIdToBackend(String regid) {
+        Log.i(TAG, "test");
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpPost httpPost = new HttpPost("http://mysterious-gorge-5071.herokuapp.com/register");
+
+        try {
+            // Add your data
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+            nameValuePairs.add(new BasicNameValuePair("id", regid));
+            Log.i(TAG, String.valueOf(nameValuePairs));
+            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+            // Execute HTTP Post Request
+            HttpResponse response = httpClient.execute(httpPost);
+
+        } catch (ClientProtocolException e) {
+            Log.i(TAG, "failed");
+
+            // TODO Auto-generated catch block
+        } catch (IOException e) {
+            Log.e(TAG, "ioexception", e);
+
+            // TODO Auto-generated catch block
+        }
     }
 
 
@@ -251,6 +292,141 @@ public class MainActivity extends Activity {
             return false;
         }
         return true;
+    }
+
+    public static class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // Explicitly specify that GcmIntentService will handle the intent.
+            ComponentName comp = new ComponentName(context.getPackageName(),
+                    GcmIntentService.class.getName());
+            // Start the service, keeping the device awake while it is launching.
+            startWakefulService(context, (intent.setComponent(comp)));
+            setResultCode(Activity.RESULT_OK);
+        }
+    }
+
+    public static class GcmIntentService extends IntentService {
+        public static final int NOTIFICATION_ID = 1;
+        private NotificationManager mNotificationManager;
+        NotificationCompat.Builder builder;
+
+        public GcmIntentService() {
+            super("GcmIntentService");
+        }
+
+        @Override
+        protected void onHandleIntent(Intent intent) {
+            Log.i(TAG, "lolololol");
+            Bundle extras = intent.getExtras();
+            GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
+            // The getMessageType() intent parameter must be the intent you received
+            // in your BroadcastReceiver.
+            String messageType = gcm.getMessageType(intent);
+
+            if (!extras.isEmpty()) {  // has effect of unparcelling Bundle
+            /*
+             * Filter messages based on message type. Since it is likely that GCM
+             * will be extended in the future with new message types, just ignore
+             * any message types you're not interested in, or that you don't
+             * recognize.
+             */
+                if (GoogleCloudMessaging.
+                        MESSAGE_TYPE_SEND_ERROR.equals(messageType)) {
+                    sendNotification("Send error: " + extras.toString());
+                } else if (GoogleCloudMessaging.
+                        MESSAGE_TYPE_DELETED.equals(messageType)) {
+                    sendNotification("Deleted messages on server: " +
+                            extras.toString());
+                    // If it's a regular GCM message, do some work.
+                } else if (GoogleCloudMessaging.
+                        MESSAGE_TYPE_MESSAGE.equals(messageType)) {
+                    Log.i(TAG, "Completed work @ " + SystemClock.elapsedRealtime());
+                    // Post notification of received message.
+
+
+                    String message = extras.getString("message") + extras.getString("temperature");
+
+
+                    sendNotification2("Received: " + message);
+                    Log.i(TAG, "Received: " + extras.toString());
+                }
+            }
+            // Release the wake lock provided by the WakefulBroadcastReceiver.
+            GcmBroadcastReceiver.completeWakefulIntent(intent);
+        }
+
+        // Put the message into a notification and post it.
+        // This is just one simple example of what you might choose to do with
+        // a GCM message.
+        private void sendNotification(String msg) {
+            Log.i(TAG, msg);
+            mNotificationManager = (NotificationManager)
+                    this.getSystemService(Context.NOTIFICATION_SERVICE);
+
+            PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                    new Intent(this, MainActivity.class), 0);
+
+            NotificationCompat.Builder mBuilder =
+                    new NotificationCompat.Builder(this)
+                            .setContentTitle("GCM Notification")
+                            .setStyle(new NotificationCompat.BigTextStyle()
+                                    .bigText(msg))
+                            .setContentText(msg);
+
+            mBuilder.setContentIntent(contentIntent);
+            mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+        }
+
+        private void sendNotification2(String msg) {
+
+            // Bundle[{from=964216151419, error:100, temperature=10, message=hello world}]
+            Bundle bundle = new Bundle();
+            bundle.putDouble("from", new Double("964216151419"));
+            bundle.putInt("error", 100);
+            bundle.putInt("temperature", 10);
+            bundle.putString("message", "hello world");
+
+            Intent appearinIntent = new Intent(Intent.ACTION_VIEW);
+
+            appearinIntent.setComponent(ComponentName.unflattenFromString("com.android.chrome/com.android.chrome.Main"));
+            appearinIntent.addCategory("android.intent.category.LAUNCHER");
+            appearinIntent.setData(Uri.parse("https://appear.in/my-beer-is-ruined"));
+
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, appearinIntent, 0);
+
+            NotificationCompat.Builder mBuilder =
+                    new NotificationCompat.Builder(this)
+                            .setSmallIcon(R.drawable.icon)
+                            .setContentTitle("Temperature alert")
+                            .setContentText(msg)
+                            .addAction(R.drawable.appearin, "Appear", pendingIntent);
+
+
+            Intent resultIntent = new Intent(this, ResultActivity.class);
+
+            // The stack builder object will contain an artificial back stack for the
+            // started Activity.
+            // This ensures that navigating backward from the Activity leads out of
+            // your application to the Home screen.
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+            // Adds the back stack for the Intent (but not the Intent itself)
+            stackBuilder.addParentStack(ResultActivity.class);
+            // Adds the Intent that starts the Activity to the top of the stack
+            stackBuilder.addNextIntent(resultIntent);
+            PendingIntent resultPendingIntent =
+                    stackBuilder.getPendingIntent(
+                            0,
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                    );
+            mBuilder.setContentIntent(resultPendingIntent);
+            NotificationManager mNotificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            // mId allows you to update the notification later on.
+            mNotificationManager.notify(1234, mBuilder.build());
+        }
+
+
     }
 
 
